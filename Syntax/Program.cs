1afce7536c2995ade3ae7f120c5e3bd39abc336b
cristianespinosa7621@gmail.com <email@example.com>
2022-06-03 09:48:33 -0700

    initial commit

---
+++ Syntax/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var answer = 4;
+            
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than 9";
+            Console.WriteLine(response);
+ 
+        }
+    }
+}
+
